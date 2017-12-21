b25bfc4b06c73201b3a4cce7b3de541241e3eeab
Ben Keil <benkeil.me@googlemail.com>
2017-12-21 01:30:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Neo4j Sample
+
+http://neo4j.com/docs/developer-manual/current/
+
+## Requirements
+
+    ```bash
+    brew install asciidoctor
+    sudo gem install tilt thread_safe
+    ```
+
+## Cypher Query Language
---
+++ render.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR=${0%%/run.sh}
+
+function render {
+    ADOC=${1}
+    shift
+    HTML=${ADOC%.*}.html
+    HTML=html/${HTML##*/}
+    HTML=${1-$HTML}
+    shift
+    OFFSET=${1-+1}
+    shift
+    BASE_URL="https://github.com/benkeil/neo4j-sample"
+    shift
+    echo rendering $ADOC to $HTML   
+    echo asciidoctor $ADOC -T $DIR/templates -a allow-uri-read -a experimental -a guides=$BASE_URL -a current=$BASE_URL -a img=$BASE_URL/img -a leveloffset=${OFFSET} -a env-guide= -a guide= -o ${HTML} "$@"
+    asciidoctor $ADOC -T $DIR/templates -a allow-uri-read -a guides=$BASE_URL -a icons=font -a current=$BASE_URL -a img=$BASE_URL/img -a leveloffset=${OFFSET} -a env-guide= -a guide= -o ${HTML} "$@"
+}
+
+render "$@"
